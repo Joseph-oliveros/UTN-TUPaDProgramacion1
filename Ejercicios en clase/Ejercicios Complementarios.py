72efdb0cfb0df715b33bb964ebaaea65426c2065
Joseph-oliveros <oliverosezequiel0@gmail.com>
2025-08-25 21:00:57 -0300

    subo ejercicio condicionales

---
+++ Ejercicios en clase/Ejercicios Complementarios.py
@@ -0,0 +1,80 @@
+#EJERCICIO 1
+
+numero1=10
+
+#EJERCICIO 2
+
+numero2= 2.5
+
+#EJERCICIO 3
+
+suma=numero1+numero2
+
+#EJERCICIO 4
+
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+print(f"Resta= {resta}, Multiplicacion= {multiplicacion}\
+, Division= {division}")
+
+#EJERCICIO 5
+
+nombre=("Joseph Oliveros")
+
+#EJERCICIO 6
+
+precio=1999.99
+
+#EJERCICIO 7
+
+descuento=0.25
+
+#EJERCICIO 8
+
+precio_descuento=precio*descuento
+precio_final=precio-precio_descuento
+print(f"El precio final es {precio_final:.2f}")
+
+#EJERCICIO 9
+
+cadena=("Aprendiendo en Python")
+
+#EJERCICIO 10
+
+longitud=len(cadena)
+
+#EJERCICIO 11
+
+precio2=int(50.10)
+
+#EJERCICIO 12
+
+nombre=("Joseph")
+apellido=("Oliveros")
+nombre_completo=(f"{nombre} {apellido}")
+
+#EJERCICIO 13
+
+edad=24
+edad=(edad+5)
+edad=(edad-10)
+print(edad)
+
+#EJERCICIO 14
+
+altura=1.87
+altura=(altura*4)
+altura=(altura/3)
+print (altura)
+
+#EJERCICIO 15
+
+nombre_mayusculas=("JOSEPH OLIVEROS")
+nombre_minusculas=nombre_mayusculas.lower()
+print(nombre_minusculas)
+
+#EJERCICIO 16
+
+nombre_letra=nombre_mayusculas.capitalize()
+print(nombre_letra)
